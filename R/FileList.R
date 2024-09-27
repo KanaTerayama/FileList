dd7bf1f1fc9fc5a8c576bffcbc849a6cf803089b
KanaTerayama <kana@u-ga.net>
2024-09-27 13:24:38 +0900

    update

--- R/FileList.R
+++ R/FileList.R
@@ -13,24 +13,15 @@ FileList<-function(path,tmstamp){
 
   for (i in 1:dir.l) {
     dir.n<-dir.list[i]
-    file.name<-list.files(paste(path,"\\",dir.n,sep=""))
+    file.name<-list.files(paste(set.dir,"\\",dir.n,sep=""))
     file.name<-file.name[!grepl(".csv",file.name)]
-    file.DF<-file.info(paste(path,"\\",dir.n,"\\",file.name,sep=""))
-    datetime<-dplyr::select(file.DF,tmstamp)
-    datetime2<-strsplit(as.character(datetime[,1])," ")
-    Tm.L<-length(datetime2)
-    timelist=list()
-    for (tm in 1:Tm.L) {
-      time2<-as.character(datetime2[[tm]][2])
-      time2<-strsplit(as.character(datetime2[[tm]][2]),"[.]")
-      TDF<-data.frame(date=as.character(datetime2[[tm]][1]),
-                      time=as.character(time2[[1]][1]))
-      timelist[[tm]]<-TDF
-    }
-    timeDF=do.call(rbind,timelist)
+    file.DF<-file.info(paste(set.dir,"\\",dir.n,"\\",file.name,sep=""))%>%
+      separate(tmstamp,c("date","time"),sep=" ")%>%
+      mutate(filename=file.name,date=date,time=time)%>%
+      select(filename,date,time)
     fileNDF<-data.frame(filename=file.name,
-                        date=timeDF$date,
-                        time=timeDF$time)
+                        date=file.DF$date,
+                        time=file.DF$time)
     write.csv(fileNDF,row.names = F,paste(path,"\\",dir.n,"\\",dir.n,".csv",sep=""))
   }
   print("csv files were created")
